0c01aae99dc3bfbb593d37b4caeb297999a2f467
Zack McKenna <zackmckenna@gmail.com>
2020-08-10 12:24:37 -0400

    inital commit

---
+++ Scene1.js
@@ -0,0 +1,10 @@
+class Scene1 extends Phaser.Scene {
+  constructor(){
+    super("bootGame")
+  }
+
+  create() {
+    this.add.text(20,20, "loading game....")
+    this.scene.start("playGame")
+  }
+}
---
+++ game.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+
+const config = {
+  width: 800,
+  height: 600,
+  backgroundColor: 0x000000,
+  scene: [Scene1, Scene2]
+}
+
+  const game = new Phaser.Game(config);
+}
+
